74b6185166e0fa46155cbf28de52b100aa4e56aa
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-06-13 00:09:06 +0300

    Выполнили задачу №2 Семинара №1

---
+++ Tasks/Task002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число №1: ");
+String? input1 =System.Console.ReadLine();
+int num1 = int.Parse(input1!);
+
+Console.WriteLine("Введите число №2: ");
+String? input2 =System.Console.ReadLine();
+int num2 = int.Parse(input2!);
+
+if(num1<num2) { 
+System.Console.WriteLine($"{num2} больше {num1}");
+} 
+else  {
+System.Console.WriteLine($"{num1} больше {num2}");
+}
